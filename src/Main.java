0d87d6a387d778dbf132449ce9e69720ec0ccfb8
Dhananjay <dhananjay.pandey72@gmail.com>
2026-02-11 21:03:39 +0530

    Iterate using list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<String> name = Arrays.asList("Java","C++","PHP","C#");
 
+        name.forEach(System.out::println);
     }
 }
